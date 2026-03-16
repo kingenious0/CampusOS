4d3ba6494d8fce55b512f2d4411bd5362006a4d6
kingenious0-8358 <kingenious0@gmail.com>
2026-03-16 15:41:06 +0000

    feat: 46-location campus map  correct coords + new Transport, Credit Union, ATM, courts, Franky Jay, UBA, Autonomy Hall

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Caches app shell + tile images for offline use
  */
 
-const CACHE_NAME  = 'campusos-v28';
+const CACHE_NAME  = 'campusos-v31';
 const TILE_CACHE  = 'campusos-tiles-v1';
 
 // App shell files to cache on install
